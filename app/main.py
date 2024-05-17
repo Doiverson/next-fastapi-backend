cf94b3ac15d6f612668c2cc8cd29609365b6324e
Doiverson <doiverson.canada@gmail.com>
2024-05-16 22:55:08 -0700

    Update .gitignore and add CORS middleware to main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .models import models
 from .database import engine
 from .routers import posts
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World ALL"}
+    return {"message": "API is successfully connected"}
 
 
 app.include_router(posts.router)
